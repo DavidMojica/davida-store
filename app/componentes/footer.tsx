e02ddd9fa6a2e9d14dae23474ee4406bf9ebd414
David Mojica <davidmojicav@gmail.com>
2024-06-11 16:12:56 -0500

    Responsive design

--- app/componentes/footer.tsx
+++ app/componentes/footer.tsx
@@ -6,50 +6,51 @@ const Footer = ():React.JSX.Element => {
   return (
     <footer className='bg-white dark:bg-black p-4'>
         <section className='text-center'>
-            <p className='font-bold text-xl flex-wrap w-1/2 m-auto'>
+            <p className='font-bold text-sm md:text-xl flex-wrap w-4/5 md:w-1/2 m-auto text-black dark:text-white'>
                 Hola a todos! Somos una tienda virtual de productos varios con envío a toda Colombia. Esperamos que les gusten nuestros productos.
             </p>
         </section>
-        <section className='flex justify-evenly mt-4'>
+        <section className='md:flex md:justify-evenly mt-4'>
             <article>
-                <img src="/logo.png" alt="logo_light" className='block dark:hidden h-48 w-48 rounded-full scale-125' />
-                <img src="/logo_dark.png" alt="logo_dark" className=' hidden dark:block h-48 w-48 rounded-full scale-125' />
+                <img src="/logo.png" alt="logo_light" className='block dark:hidden h-48 w-48 rounded-full m-auto md:scale-125' />
+                <img src="/logo_dark.png" alt="logo_dark" className=' hidden dark:block h-48 w-48 rounded-full m-auto md:scale-125' />
             </article>
             <article className='text-center '>
                 <h2 className='text-2xl text-red-700 dark:text-red-900 font-bold'>Nuestro Sitio</h2>
-                <ul className=''>
-                    <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Home</Link></li>
-                    <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Catálogo</Link></li>
-                    <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Conócenos</Link></li>
-                    <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Contáctenos</Link></li>
+                <ul className='text-black dark:text-white'>
+                    <li><Link href={'/'} className='mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Home</Link></li>
+                    <li><Link href={'/'} className='mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Catálogo</Link></li>
+                    <li><Link href={'/'} className='mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Conócenos</Link></li>
+                    <li><Link href={'/'} className='mx-4 font-medium duration-300 text-md transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Contáctenos</Link></li>
                 </ul>
             </article>
             <article className='flex items-center text-center'>
-                <p className=''>
+                <p className='text-black dark:text-white font-bold m-auto '>
                     ¡También somos empresa de desarrollo! <br />
-                    <Link href={'/'} className='text-red-700 hover:text-red-900 dark:text-red-900 dark:hover:text-red-700'>Cotice sus sitos web y apliaciones con nosotros </Link>
-                    
+                    <Link href={'/'} className='text-red-700 hover:text-red-900 dark:text-red-900 dark:hover:text-red-700'>
+                        Cotice sus sitos web con nosotros 
+                    </Link>
                 </p>
             </article>
         </section>
         <section className='flex justify-evenly mt-4'>
             <article>
-                <h2 className='text-2xl font-bold'>Área de ventas</h2>
+                <h2 className='text-2xl font-bold text-black dark:text-white text-center'>Área de ventas</h2>
                 <div className='flex justify-evenly mt-2'>
                     <IconFactory.Whatsapp size='w-12 h-12' padding='p-2' />   
                     <IconFactory.Facebook size='w-12 h-12' padding='p-2' />   
                 </div>    
             </article>
             <article >
-                <h2 className='text-2xl font-bold'>Área de desarrollo</h2>
+                <h2 className='text-2xl font-bold text-black dark:text-white text-center'>Área de desarrollo</h2>
                 <div className='flex justify-evenly mt-2'>
                     <IconFactory.Whatsapp size='w-12 h-12' padding='p-2' />
                     <IconFactory.GitHub size='w-12 h-12' padding='p-2' />
                 </div>
             </article>
         </section>
-        <section className='text-center mt-4'>
-            <h4 className='text-xl'>Copyrigth 2024 - Desarrollado por: David Mojica</h4>
+        <section className='text-center mt-4 font-bold'>
+            <h4 className='text-xl text-black dark:text-white'>Copyrigth 2024 - Desarrollado por: David Mojica</h4>
         </section>
     </footer>
   )
--- app/componentes/header.tsx
+++ app/componentes/header.tsx
@@ -4,17 +4,17 @@ import Switch_day from './switch_day';
 
 const Header = ():React.JSX.Element => {
   return (
-    <header className='grid sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center p-4 bg-black'>
+    <header className='grid sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center p-4 bg-white dark:bg-black'>
         <section className='sm:flex sm:items-center'>
             <img src="/logo.png" alt="logo_light" className='block dark:hidden h-48 w-48 rounded-full scale-125' />
             <img src="/logo_dark.png" alt="logo_dark" className=' hidden dark:block h-48 w-48 rounded-full scale-125' />
         </section>
         <section className='sm:col-span-1 md:col-span-2'>
             <ul className='sm:block md:flex text-center md:divide-x-2 md:divide-black md:dark:divide-white'>
-                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Home</Link></li>
-                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Catálogo</Link></li>
-                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Conócenos</Link></li>
-                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Contáctenos</Link></li>
+                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-lg md:text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Home</Link></li>
+                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-lg md:text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Catálogo</Link></li>
+                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-lg md:text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Conócenos</Link></li>
+                <li><Link href={'/'} className='text-black dark:text-white mx-4 font-medium duration-300 text-lg md:text-3xl transition-all ease-in-out hover:underline hover:decoration-red-700 hover:text-red-700 dark:hover:decoration-red-900 dark:hover:text-red-900 hover:underline-offset-4'>Contáctenos</Link></li>
             </ul>
         </section>
         <section className='hidden md:block'>
